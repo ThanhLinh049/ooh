683c187281ecfd735d50798c6e2a54114e6d58f0
thanhlinh049 <thanhlinh929@gmail.com>
2026-06-03 15:45:03 +0700

    Fix footer

--- src/components/site/Footer.tsx
+++ src/components/site/Footer.tsx
@@ -1,5 +1,5 @@
 import { Mail, Phone, MapPin } from "lucide-react";
-import Logo from "@/assets/logo-vieclamooh-white.png";
+import Logo from "@/assets/Logo-Vieclamooh-white.png";
 
 const cols = [
   {
